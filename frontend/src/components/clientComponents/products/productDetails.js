ff37a776a57cb6f22bb3aa64eb96aecea6c89659
Aakash <aakash2102.gupta@gmail.com>
2020-04-08 18:46:53 -0700

    adding components for different user type

---
+++ frontend/src/components/clientComponents/products/productDetails.js
@@ -0,0 +1,18 @@
+import React,{ Component }from 'react';
+// import axios from 'axios';
+// import Button from '@material-ui/core/Button';
+
+class productDetails extends Component {
+    
+    render() {
+      return (
+        <div>
+          <p> The prod details page </p>
+          
+        </div>
+       
+      );
+    }
+  }
+
+  export default productDetails;
