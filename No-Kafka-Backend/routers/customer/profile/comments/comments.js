dd8cf8bff4988593f1ef56c914367546f6ed27be
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-04-19 21:02:56 -0700

    added backend routes

---
+++ No-Kafka-Backend/routers/customer/profile/comments/comments.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const router = express.Router();
+
+
+router.get('/',(req,res)=>{
+    
+})
+
+router.post('/',(req,res)=>{
+    
+})
+
+router.put('/',(req,res)=>{
+   
+})
+
+router.delete('/',(req,res)=>{
+    
+})
+
+
+module.exports=router;
---
+++ No-Kafka-Backend/routers/seller/seller.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router = express.Router();
+const kafka= require('./../../kafka/client')
+
+
+router.get('/',(req,res)=>{
+    
+})
+
+module.exports=router;
