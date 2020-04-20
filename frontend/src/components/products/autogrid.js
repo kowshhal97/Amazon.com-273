d20eb033b78c9a6990601845d8d19744ecf69a95
ChaitanyaDasari <saichaitanya52@gmail.com>
2020-04-20 15:36:41 -0700

    size dropdown

--- frontend/src/components/products/autogrid.js
+++ frontend/src/components/products/autogrid.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import GridProductsImage from '../products/gridProductimages';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import AddToCart from '../products/addToCart';
 import Rating from '@material-ui/lab/Rating';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,32 +18,55 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 
+
  const AutoGrid = () => {
-   
-  const [value, setValue] = React.useState(2);
 
   const classes = useStyles();
+  const [value, setValue] = React.useState(2);
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  
+ 
 
   return (
     <div className={classes.root}>
        
       <Grid container spacing={3}>
-      {/* <Paper className={classes.paper}> */}
+     
         <Grid item xs>  
               <GridProductsImage/>             
         </Grid>
-        {/* </Paper>  */}
+      
         <Grid  alignItems="flex-start" item xs >
-        {/* <div>product deatils description
-                </div> */}
           <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             Apple
           </Typography>
           <Rating name="read-only" value={value} readOnly />
+          <p> Price :  </p>
+          <p> Size :  </p>
+
+          <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+        </Select>
+          
+
           <Typography variant="body2" color="textSecondary" component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
             across all continents except Antarctica is this all enoug the data we have the mannaher jk
@@ -50,6 +74,7 @@ const useStyles = makeStyles((theme) => ({
           </Typography>
         </CardContent>
         </Grid>
+        
         <div> 
         <AddToCart />
           </div>
