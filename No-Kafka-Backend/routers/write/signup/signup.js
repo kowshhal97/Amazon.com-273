5a2b1f5c9eed5859660962b9d72a95410a9fa49e
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-04-20 16:23:21 -0700

    login and signup api

--- No-Kafka-Backend/routers/write/signup/signup.js
+++ No-Kafka-Backend/routers/write/signup/signup.js
@@ -1,12 +1,26 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
+const User = require('./../../../mysqlModels/User')
 
 
 
-router.post('/',(req,res)=>{
-    //signup code
+router.post('/', async (req, res) => {
+
+    try {
+        const user = await User.create({
+            email: req.body.email,
+            password: req.body.password,
+            userType: req.body.userType
+        })
+        return res.status(200).send(user);
+    }
+    catch (err) {
+         console.log(err);
+    }
+    return res.status(500).send("Internal Server Error!");
+
 })
 
 
 
-module.exports=router;
+module.exports = router;
