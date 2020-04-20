8bc8e8eac477f1d5a00487ad0736679bc2e6723e
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-04-20 16:50:39 -0700

    updated schema and login signup api

--- No-Kafka-Backend/routers/write/signup/signup.js
+++ No-Kafka-Backend/routers/write/signup/signup.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./../../../mysqlModels/User')
-
-
+const User = require('./../../../mysqlModels/User');
+const Customer = require('./../../../mysqlModels/Customer');
+const Seller = require('./../../../mysqlModels/Seller');
+const Admin = require('./../../../mysqlModels/Admin');
 
 router.post('/', async (req, res) => {
 
@@ -12,10 +13,31 @@ router.post('/', async (req, res) => {
             password: req.body.password,
             userType: req.body.userType
         })
+        if (user.userType === "customer") {
+            Customer.create({
+                name: req.body.name
+            }).then(customer => {
+                customer.setUser(user)
+            });
+        }
+        else if (user.userType === "seller") {
+            Seller.create({
+                name: req.body.name
+            }).then(seller => {
+                seller.setUser(user)
+            });
+        }
+        else {
+            Admin.create({
+                name: req.body.name
+            }).then(admin => {
+                admin.setUser(user)
+            });
+        }
         return res.status(200).send(user);
     }
     catch (err) {
-         console.log(err);
+        console.log(err);
     }
     return res.status(500).send("Internal Server Error!");
 
