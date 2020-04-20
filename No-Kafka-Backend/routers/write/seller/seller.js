3d045eecde01a411cae65a0c768d1849c23e4667
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-04-20 11:24:23 -0700

    updated routes to read write split

--- No-Kafka-Backend/routers/write/seller/seller.js
+++ No-Kafka-Backend/routers/write/seller/seller.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const router = express.Router();
 
-const products=require('./products/products');
+
 const profile=require('./profile/profile');
 
-router.use('/product',products);
+router.use('/profile',profile);
 
 module.exports=router;
