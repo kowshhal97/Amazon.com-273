6763a0a300d216ac05cb6e95104ee7c2c5d36390
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-04-20 00:05:15 -0700

    added models

---
+++ No-Kafka-Backend/models/CustomerAddress.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../db/SQLdatabase');
+
+const CustomerAddress = sequelize.define('customerAddress', {
+  firstName: {
+    type: Sequelize.STRING,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+  },
+  address1: {
+    type: Sequelize.STRING,
+  },
+  address2: {
+    type: Sequelize.STRING,
+  },
+  city: {
+    type: Sequelize.STRING,
+  },
+  state: {
+    type: Sequelize.STRING,
+  },
+  country: {
+    type: Sequelize.STRING,
+  },
+  zipcode: {
+    type: Sequelize.STRING,
+  },
+  phoneNumber: {
+    type: Sequelize.STRING,
+  },
+});
+
+module.exports = CustomerAddress;
