950bce8843894d10a225e0fbfe69df2d809535d1
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-04-21 02:32:46 -0700

    modified post product route

--- No-Kafka-Backend/routers/write/products/products.js
+++ No-Kafka-Backend/routers/write/products/products.js
@@ -1,10 +1,33 @@
 const express=require('express');
 const router = express.Router();
+const Seller = require('../../../mysqlModels/Seller');
+const Product = require('../../../mysqlModels/Product');
 
 
 
-router.post('/',(req,res)=>{
-    
+router.post('/', async (req,res)=>{
+    const sellerId = req.body.id;
+    try {
+        const seller = await Seller.findOne({
+            where: {
+                id: sellerId
+            }
+        })
+        if(seller === null) {
+            return res.status(404).send('User not found!');
+        }
+        const product = await Product.create({
+            productName: req.body.productName,
+            sellerName: req.body.sellerName,
+            price: req.body.price,
+            rating: req.body.rating,
+            description: req.body.description
+        })
+        return res.status(200).send(product);
+    } catch(err) {
+        console.log(err)
+        return res.status(500).send('Internal Server Error!');
+    }
 })
 
 router.put('/:id',(req,res)=>{
