6b2b82b43d1458dd77c9ce1ec95b2a7930bb242a
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-04-21 16:40:59 -0700

    added average rating

--- No-Kafka-Backend/routers/read/products/products.js
+++ No-Kafka-Backend/routers/read/products/products.js
@@ -1,36 +1,48 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
-const Product=require('./../../../mysqlModels/Product');
+const Product = require('./../../../mysqlModels/Product');
+const Votes = require('./../../../mysqlModels/votes')
+
+const sequelize = require('sequelize')
 
 
-router.get('/',async (req,res)=>{
+router.get('/', async (req, res) => {
     try {
         const products = await Product.findAll();
         return res.status(200).send(products);
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.status(500).send('Internal Server Error!');
     }
 })
 
-router.get('/:sellerId',async(req,res)=>{
+router.get('/seller/:sellerId', async (req, res) => {
     const id = req.params.sellerId;
-  try {
-    const products = await Seller.findAll({
-      where: {
-        sellerId: id,
-      },
-    });
-    if (products === null) {
-      return res.status(404).send('User not found!');
+    try {
+        const products = await Seller.findAll({
+            where: {
+                sellerId: id,
+            },
+        });
+        if (products === null) {
+            return res.status(404).send('User not found!');
+        }
+        return res.status(200).send(products);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send('Internal Server Error!');
     }
-    return res.status(200).send(products);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send('Internal Server Error!');
-  }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    res.send(await Votes.findAll({
+        where: { id: id },
+        attributes: ['productId', [sequelize.fn('AVG',
+            sequelize.col('rating')), 'ratingAvg']],
+    }))
 })
 
 
-
-module.exports=router;
+module.exports = router;
