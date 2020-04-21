76ede6d004fe1f6f141d25615dc212a00d28eb32
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-04-21 15:19:02 -0700

    updated products get

--- No-Kafka-Backend/routers/read/products/products.js
+++ No-Kafka-Backend/routers/read/products/products.js
@@ -1,13 +1,34 @@
 const express=require('express');
 const router = express.Router();
+const Product=require('./../../../mysqlModels/Product');
 
 
-router.get('/',(req,res)=>{
-    
+router.get('/',async (req,res)=>{
+    try {
+        const products = await Product.findAll();
+        return res.status(200).send(products);
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send('Internal Server Error!');
+    }
 })
 
-router.get('/:sellerId',(req,res)=>{
-    
+router.get('/:sellerId',async(req,res)=>{
+    const id = req.params.sellerId;
+  try {
+    const products = await Seller.findAll({
+      where: {
+        sellerId: id,
+      },
+    });
+    if (products === null) {
+      return res.status(404).send('User not found!');
+    }
+    return res.status(200).send(products);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send('Internal Server Error!');
+  }
 })
 
 
