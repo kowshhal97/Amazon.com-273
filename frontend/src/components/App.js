16215998b1ac8f4f9ebbb0300f09a7e35fe9f656
ChaitanyaDasari <saichaitanya52@gmail.com>
2020-04-20 17:41:01 -0700

    signInPage

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,8 @@ import Products from './products/products';
 import ProductDetails from './products/productDetails';
 import Home from './Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SignUp from './SignUp/SingUp';
+import SignIn from './Login/SingIn';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       
       
       <div>
+         <Route path='/signup' component={SignUp} />
+         <Route path='/signin' component={SignIn} />
         <Route path='/user/products' component={Products} />
         <Route path='/user/product/Productdetails' component={ProductDetails} />
   
