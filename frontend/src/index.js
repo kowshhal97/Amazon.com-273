b478f67aea60c1c8a89a1dc89259694e06c4bc2e
= <ambika@sjsu.edu>
2020-04-21 23:37:07 -0700

    adding react bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from './store/reducers/index';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk),
