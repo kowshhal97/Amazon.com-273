fef4bdf14a35c08274a7a4ff66e613c81ad25fc8
= <ambika@sjsu.edu>
2020-04-23 22:40:06 -0700

    adding bootstrap and intital redux store

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,24 @@ import Products from './clientComponents/products/products';
 import ProductDetails from './clientComponents/products/productDetails';
 import cart from './clientComponents/cartComp/cart';
 import Home from './Home';
+//import header from './clientComponents/orders/orderHeader';
+import Orders from './clientComponents/orders/orders';
+import CancelledOrders from './clientComponents/orders/cancelledOrders';
+import OpenOrders from './clientComponents/orders/openOrders';
+import OrderDetails from './clientComponents/orders/orderDetails';
+import CancelOrder from './clientComponents/orders/cancelOrder';
+import OrderStatus from './clientComponents/orders/orderStatus';
+
+
+//Seller Components
+import SellerOrders from './sellerComponents/orders/orders';
+import SellerCancelledOrders from './sellerComponents/orders/cancelledDelivered';
+import SellerOpenOrders from './sellerComponents/orders/open';
+
+
+//Admin Components
+import AdminOrders from './adminComponents/orders/orders';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
@@ -11,13 +29,29 @@ const App = () => {
       <div>
       <Route exact path="/" component={Home} />
       </div>
-      
-      
       <div>
         
         <Route path='/user/products' component={Products} />
         <Route path='/user/product/Productdetails' component={ProductDetails} />
         <Route path='/user/cart/' component={cart} />
+        <Route path='/user/orders/details/' component={OrderDetails} />
+        <Route exact path='/user/orders/' component={Orders} />
+
+        <Route path='/user/orders/cancelOrder/' component={CancelOrder} />
+        <Route exact path='/user/open/' component={OpenOrders} />
+        <Route path='/user/cancelled/' component={CancelledOrders} />
+        <Route path='/user/orders/orderStatus/' component={OrderStatus} />
+
+
+        <Route exact path='/seller/orders/' component={SellerOrders} />
+        <Route path='/seller/cancelledDelivered/' component={SellerCancelledOrders} />
+        <Route path='/seller/open/' component={SellerOpenOrders} />
+
+        <Route exact path='/admin/orders/' component={AdminOrders} />
+
+
+
+
 
   
       </div>
