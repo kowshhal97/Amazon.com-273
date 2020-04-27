3c5e797163d6014740786dc2163bfcbd0ac2f5e3
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-04-26 20:00:42 -0700

    updated cart nad product api

--- No-Kafka-Backend/routers/read/customer/cart/cart.js
+++ No-Kafka-Backend/routers/read/customer/cart/cart.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const Cart = require('./../../../../mysqlModels/Cart')
 const Product = require('./../../../../mysqlModels/Product')
-
+const ProductImages = require('./../../../../mysqlModels/productImages')
 
 router.get('/:userId', async (req, res) => {
     try {
@@ -12,10 +12,11 @@ router.get('/:userId', async (req, res) => {
 
         const product = await Product.findOne({
             where:{
-                id:req.params.id
+                id:cart.productId
             },include: [{ model:ProductImages,as:'productImages'}]});
 
-        var output={...cart,...product}
+            console.log(product)
+        var output={...cart.dataValues,...product.dataValues}
         return res.status(200).send(output);
     }
     catch (err) {
