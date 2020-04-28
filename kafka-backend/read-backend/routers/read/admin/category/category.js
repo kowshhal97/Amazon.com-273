b7d2b17ea80e64bbdb590e2b0aa99c5a17062084
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-04-28 02:07:14 -0700

    modified adminCategory service in read

--- kafka-backend/read-backend/routers/read/admin/category/category.js
+++ kafka-backend/read-backend/routers/read/admin/category/category.js
@@ -5,31 +5,32 @@ const Category = require('../../../../mysqlModels/Category');
 
 
 router.get('/', async (req, res) => {
-    try {
-        const categories = await Category.findAll();
-        return res.status(200).send(categories);
-    } catch (err) {
-        console.log(err);
-        return res.status(500).send('Internal Servre Error!')
-    }
+    req.body.path = 'getAllCategoryHandler'
+    kafka.make_request('admin-category-read', req.body, (err, results) => {
+ 
+        console.log(results)
+         res.status(results.status).send(JSON.parse(results.data));
+    
+      });
+    // try {
+    //     const categories = await Category.findAll();
+    //     return res.status(200).send(categories);
+    // } catch (err) {
+    //     console.log(err);
+    //     return res.status(500).send('Internal Servre Error!')
+    // }
 })
 
 router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const category = await Category.findOne({
-            where: {
-                id: id
-            }
-        })
-        if(category === null) {
-            return res.status(404).send('Category not found!');
-        }
-        res.status(200).send(category);
-    } catch(err) {
-        console.log(err);
-        return res.status(500).send('Internal Servre Error!')
-    }
+    req.body.id = req.params.id;
+    req.body.path = 'getCategoryByIdHandler'
+    kafka.make_request('admin-category-read', req.body, (err, results) => {
+ 
+        console.log(results)
+         res.status(results.status).send(JSON.parse(results.data));
+    
+      });
+   
     
 })
 
--- kafka-read-backend/services/adminCategory.js
+++ kafka-read-backend/services/adminCategory.js
@@ -1,11 +1,43 @@
 const Category = require('../mysqlModels/Category');
 
 getAllCategoryHandler = (msg, callback) => {
-
+    var res = {}
+    try {
+        const categories = await Category.findAll();
+        res.status = 200;
+        res.data = JSON.stringify(categories);
+        callback(null, res)
+    } catch (err) {
+        console.log(err);
+        res.status = 500
+        res.data = 'Internal Servre Error!'
+        callback(null, res);
+        
+    }
 }
 
 getCategoryByIdHandler = (msg, callback) => {
-
+    var res = {};
+    const id = msg.id;
+    try {
+        const category = await Category.findOne({
+            where: {
+                id: id
+            }
+        })
+        if(category === null) {
+            res.status = 404
+            res.data = 'Category not found!';
+            callback(null, res);
+        }
+        res.status = 200
+        res.data = JSON.stringify(category);
+    } catch(err) {
+        console.log(err);
+        res.status = 500
+        res.data = 'Internal Servre Error!';
+        callback(null, res);
+    }
 }
 
 function handle_request(msg, callback) {
