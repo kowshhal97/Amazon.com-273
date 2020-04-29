4e5ba0df76d5e491368f7cae26ed6ba28ac40132
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-04-28 23:19:26 -0700

    modified adminOrder in write service

--- kafka-backend/write-backend/routers/write/admin/category/category.js
+++ kafka-backend/write-backend/routers/write/admin/category/category.js
@@ -1,47 +1,28 @@
 const express=require('express');
 const router = express.Router();
-const Category = require('../../../../mysqlModels/Category');
-
 
 router.post('/', async (req, res) => {
-    try {
-        const category = await Category.create({categoryName: req.body.categoryName});
-        return res.status(201).send(category);   
-    } catch(err) {
-        console.log(err);
-        return res.status(500).send('Internal Server Error!');
-    }
+    req.body.path = 'addCategoryHandler'
+    kafka.make_request('admin-category-write', req.body, (err, results) => {
+  
+        console.log(results)
+        res.status(results.status).send(JSON.parse(results.data));
+  
+      });
+
 })
 
-router.put('/:id',async (req, res)=>{
+router.put('/:id', async (req, res) => {
+    req.body.path = 'updateCategoryHandler'
+    req.body.id = req.params.id;
+    kafka.make_request('admin-category-write', req.body, (err, results) => {
+  
+        console.log(results)
+        res.status(results.status).send(JSON.parse(results.data));
+  
+      });
 
-    const {categoryName}=req.body
-    const id=req.params.id;
-    try {
-        const category = await Category.findOne({
-            where: {
-                id:id
-            }
-        });
-        if ( Category=== null) {
-            return res.status(404).send("Category not found!");
-        }
-        else {
-            const newCategory=await Category.update({
-               categoryName:categoryName
-            },{where:{id:id}})
-
-        return res.status(200).send(newCategory);
-        }
-    }
-    catch (err) {
-        console.log(err);
-    }
-    return res.status(500).send("Internal Server Error!");
-    
 })
-
-
 
 
 module.exports=router;
---
+++ kafka-write-backend/services/adminOrder.js
@@ -0,0 +1,45 @@
+const Order = require('../mongoModels/orders');
+
+updateOrderStatusHandler = async (msg, callback) => {
+    var res = {}
+    const { orderStatus, orderUpdateItem, productId } = msg;
+    const orderId = msg.orderId; 
+    try {
+        const order = await Order.findById({_id: orderId})
+        if(!order) {
+            res.status = 404
+            res.data = 'Order not found!';
+            callback(null, res);
+        }
+        order.products.map((product) => {
+            if(product.productId === productId) {
+                if(orderStatus) {
+                    product.orderStatus = orderStatus;
+                }
+                if(orderUpdateItem) {
+                    const list = [orderUpdateItem, ...product.orderUpdates];
+                    product.orderUpdates = list
+                }
+            }
+        })
+        await order.save();
+        res.status = 200
+        res.data = JSON.stringify(order);
+        callback(null, res) 
+    } catch(err) {
+        console.log(err);
+        res.status = 500
+        res.data = 'Internal Server Error!';
+        callback(null, res);
+    }
+}
+
+function handle_request(msg, callback) {
+    if (msg.path === 'updateOrderStatusHandler') {
+      delete msg.path
+      updateOrderStatusHandler(msg, callback)
+    }
+
+  };
+  
+  exports.handle_request = handle_request;
