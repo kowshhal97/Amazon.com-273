9559eeb37dd627f916f0aee9ef0e61b710e01a24
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-04-28 18:25:37 -0700

    modified seller product in read service

--- kafka-backend/read-backend/routers/read/seller/products/products.js
+++ kafka-backend/read-backend/routers/read/seller/products/products.js
@@ -1,24 +1,17 @@
 const express=require('express');
 const router = express.Router();
-const Product = require('./../../../../mysqlModels/Product');
 
 
-router.get('/:id',async (req,res)=>{
-    const id = req.params.id;
-    try {
-        const products = await Product.findAll({
-            where: {
-                sellerId: id,
-            },
-        });
-        if (products === null) {
-            return res.sendStatus(404);
-        }
-        return res.status(200).send(products);
-    } catch (err) {
-        console.log(err);
-        return res.sendStatus(500);
-    }
+router.get('/:id', async (req,res) => {
+    req.body.id = req.params.id;
+    req.body.path = 'getSellerProductHandler'
+    kafka.make_request('seller-products-read', req.body, (err, results) => {
+  
+        console.log(results)
+        res.status(results.status).send(JSON.parse(results.data));
+  
+      });
+    
 })
 
 module.exports=router;
