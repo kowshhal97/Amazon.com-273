76e31d8415db3707a638599caac6844ddcd5ea1a
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-05-04 01:48:15 -0700

    add cancel order admin

--- kafka-backend/write-backend/Backend/routers/write/admin/order/order.js
+++ kafka-backend/write-backend/Backend/routers/write/admin/order/order.js
@@ -15,5 +15,16 @@ router.put('/:orderId', async (req, res) => {
     
 })
 
+router.delete('/:orderId', async (req, res) => {
+  req.body.orderId = req.params.orderId;
+  req.body.path = 'cancelOrderHandler';
+  kafka.make_request('admin-order-write', req.body, (err, results) => {
+ 
+    console.log(results)
+    res.status(results.status).send(JSON.parse(results.data));
+
+  });
+})
+
 
 module.exports = router;
