98bfb8995f2a78ec918ac3f9d92f389986946187
= <ambika@sjsu.edu>
2020-05-06 11:49:08 -0700

    seller order actions and reducers

--- frontend/src/components/sellerComponents/orders/orders.js
+++ frontend/src/components/sellerComponents/orders/orders.js
@@ -9,7 +9,11 @@ import { Link } from 'react-router-dom';
 import exportData from '../../../config/config';
 import moment from 'moment';
 import Form from 'react-bootstrap/Form';
+import { connect } from 'react-redux';
+import { getSellerOrders, updateStatus } from '../../../store/actions/sellerActions/ordersActions';
 
+
+let sellerName = "OnePlus";
 class SellerOrders extends Component {
 
     state = {
@@ -18,33 +22,45 @@ class SellerOrders extends Component {
 
     }
 
+    async componentDidMount () {
+        await this.props.getSellerOrders(sellerName);
+
+    }
+
     trackPackage = () => {
 
     }
 
-    orderStatusUpdate = (e) => {
+    orderStatusUpdate = (orderDetail, e) => {
         console.log((e.target.value))
+        console.log(orderDetail);
+
+        let values={orderUpdateItem:e.target.value, productId:orderDetail.product.productId, orderId:orderDetail.orderId};
+
+        this.props.updateStatus(values);
 
         //action call
     }
 
-    changeStatus = (update) => {
-        console.log(update)
+    changeStatus = (product, orders_id) => {
+        console.log(product)
         let i =0;
-        for( i=0; i<update.length;i++);
-        console.log(i)
-        let latestStatus = update[i-1].deliveryStatus;
-        let title = exportData.deliveryStatus[latestStatus]
+        let orderDetail = {orderId: orders_id, product:product};
+        let update = product.orderUpdates;
+       // for( i=0; i<update.length;i++);
+        let latestStatus = update[0].deliveryStatus;
+        let title = exportData.deliveryStatus[latestStatus];
 
+        console.log(title)
         return (
 
-            <Form onChange={this.orderStatusUpdate}>
+            <Form onChange={(e)=>this.orderStatusUpdate(orderDetail, e)}>
                 {/* <Form.Label>Change Status</Form.Label> */}
                 <Form.Control as="select" custom>
                     <option value="3">{title}</option>
-                    {latestStatus === 0 && <option value="1">Packing</option>}
-                    {(latestStatus === 1 || latestStatus === 0) && <option value="2">Out for Shipping</option>}
-
+                    {latestStatus === "0" && <option value="1">Packing</option>}
+                    {(latestStatus === "1" || latestStatus === "0") && <option value="2">Out for Shipping</option>}
+                    <option value="2">Out for Shipping</option>
 
                 </Form.Control>
             </Form>
@@ -54,6 +70,8 @@ class SellerOrders extends Component {
 
 
     render() {
+
+        console.log(this.props.sellerOrders);
 
         let values = [{
             _id: '1',
@@ -178,7 +196,6 @@ class SellerOrders extends Component {
 
         }]
 
-        console.log(values[0].products)
         return (
             <div>
                 <div>
@@ -188,12 +205,12 @@ class SellerOrders extends Component {
                         <Row>
                             <Col md={1}></Col>
                             <Col md={10}>
-                                {values.length && values.map((orders, i) => {
+                                {this.props.sellerOrders.length && this.props.sellerOrders.map((orders, i) => {
                                     console.log(orders)
                                     return (
                                         <div key={i}>
                                             {orders.products.map((product, i) => {
-                                                if (product.sellerName === 'seller 1') {
+                                                if (product.sellerName === sellerName) {
                                                     return (
                                                         <div key={i}>
                                                             <Card>
@@ -259,14 +276,14 @@ class SellerOrders extends Component {
                                                                             <Button variant="primary" style={{ float: 'right', width: '100%', background: '#f3cf75', border: '#f3cf75', color: 'black' }}  ><Link to={{pathname:"/user/orders/orderStatus/", state:{ productInfo:product}}} style={{color:'black'}}>Track Package</Link></Button>
                                                                             <br></br>
                                                                             <br></br>
-                                                                            {product.orderStatus === 0 && <div>
-                                                                                <Button variant="primary" style={{ float: 'right', width: '100%', background: '#f0f1f4', border: '#f0f1f4', color: 'black' }}> <Link to={{ pathname: "/user/orders/cancelOrder/", state: { orderInfo: orders, productInfo: product } }} style={{ color: 'black' }}>Cancel Order</Link></Button>
+                                                                            {product.orderStatus === "0" && <div>
+                                                                                <Button variant="primary" style={{ float: 'right', width: '100%', background: '#f0f1f4', border: '#f0f1f4', color: 'black' }} > <Link to={{ pathname: "/user/orders/cancelOrder/", state: { orderInfo: orders, productInfo: product } }} style={{ color: 'black' }}>Cancel Order</Link></Button>
 
                                                                                 <br></br>
                                                                                 <br></br>
                                                                             </div>
                                                                             }
-                                                                            {this.changeStatus(product.orderUpdates)}
+                                                                            {this.changeStatus(product, orders._id)}
 
                                                                         </Col>
                                                                     </Row>
@@ -274,7 +291,6 @@ class SellerOrders extends Component {
                                                             </Card>
                                                             <br></br>
                                                         </div>
-
                                                     )
                                                 }
                                                 else {
@@ -289,13 +305,17 @@ class SellerOrders extends Component {
                             </Col>
 
                             <Col md={2}></Col>
-
                         </Row>
-
                     </Container>
                 </div>
             </div>
         )
     }
 }
-export default SellerOrders;
+
+//export default SellerOrders;
+const mapStateToProps = (state) => {
+    return { sellerOrders: state.sellerOrders }
+}
+
+export default connect(mapStateToProps, { getSellerOrders, updateStatus })(SellerOrders);
