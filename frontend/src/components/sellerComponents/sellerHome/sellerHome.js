1cd53c7a9f78b94f9443b3fc22f2c56c38e74fa0
ChaitanyaDasari <saichaitanya52@gmail.com>
2020-05-05 22:52:56 -0700

    added comments section and sellerhome page

--- frontend/src/components/sellerComponents/sellerHome/sellerHome.js
+++ frontend/src/components/sellerComponents/sellerHome/sellerHome.js
@@ -1,84 +1,101 @@
-import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
-import exportData from '../../../config/config';
-import moment from 'moment';
-import ProdctCard from '../../clientComponents/products/productCard'
-   
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
+import exportData from "../../../config/config";
+import Form from "react-bootstrap/Form";
+import ProductCard from "../../clientComponents/products/productCard";
+import "../../CSS/styles.css";
+import Header from "../../header/header";
+import { connect } from 'react-redux';
+import { getALLProductsBySeller } from '../../../store/actions/clientActions/productsActions';
 
-class UserHome extends Component {
-  constructor(props) {
-        super(props);
-        console.log(props)
-        this.state = {
-            // productInfo: this.props.location.state.productInfo
-        }
-    }
+const sell_id =1;
+class SellerHome extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true
+    };
+  }
 
-    getProducts = () => {
+async componentDidMount() {
 
-    }
+    await  this.props.getALLProductsBySeller(sell_id)
+    this.setState({
+        loading: false
+    })
+  
+  //  console.log(this.allProducts.data)
 
-    render() {
-
-        let prodvalues = [{
-                productId: 'p_1',
-                productName: 'product 1',
-                sellerName: 'seller 1',
-                quantity: 2,
-                perQuantityPrice: 1,
-                totalPrice: 2,
-                orderStatus: 0,
-                sellerid:1,
-                productPhoto:'' 
-            },
-            {
-                productId: 'p_3',
-                productName: 'product 3',
-                sellerName: 'seller 1',
-                quantity: 2,
-                perQuantityPrice: 1,
-                totalPrice: 2,
-                orderStatus: 0,
-                sellerid:1,
-                productPhoto:'' 
-            },
-            {
-                productId: 'p_2',
-                productName: 'product 2',
-                sellerName: 'seller 2',
-                quantity: 4,
-                perQuantityPrice: 1,
-                totalPrice: 2,
-                orderStatus: 0,
-                sellerid:1,
-                productPhoto:''         
-        }]
-        return (
-            <div>
-                <div>
-                   {/* <NavBar/> */}
-                    <h2>Header</h2>
-                    </div>
-                    <Container fluid>
-                       <Row> 
-                      <Col sm="3">filters</Col>
-                       <Col sm="9">
-                       <ProdctCard/>
-                       <ProdctCard/>
-                       <ProdctCard/>
-                       <ProdctCard/>
-                       </Col>
-                </Row>
-               <Row>PageNation</Row>        
-                    </Container>
-                
-            </div>
-        )
-    }
 }
-export default UserHome;
+
+displayProducts = () => {
+  //for loop
+  for(let i=0; i<this.props.allSellerProducts.length; i+=3){
+    return (<div>
+      <Row>
+        <Col md={4}>
+     { this.props.allProducts[i] && <ProductCard  cproducts = {this.props.allProducts[i]} key={i}/>}
+     </Col>
+     <Col md={4}>
+    { this.props.allProducts[i+1] && <ProductCard  cproducts = {this.props.allProducts[i+1]} key={i+1}/> }
+    </Col>
+    { this.props.allProducts[i+2] && <ProductCard  cproducts = {this.props.allProducts[i+2]} key={i+2}/>}
+    </Row>
+    </div>)
+  }
+}
+  render() {
+    const products = this.props.allSellerProducts
+    console.log(products);
+    return (
+      <div>
+        <div>
+          <Header />
+        </div>
+        <Container fluid>
+          <Row>
+            <Col md={3}>
+              <h4>Product Categories</h4>
+              <span className="block-example border border-dark">
+                {/* <Container className="themed-container"> </Container> */}
+                <Form>
+                  {["1", "2", "3", "4", "5"].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                      <Form.Check
+                        type="checkbox"
+                        id={`product-${type}`}
+                        label={` product category ${type}`}
+                      />
+                    </div>
+                  ))}
+                </Form>
+              </span>
+            </Col>
+
+            <Col md={9}>
+              {this.displayProducts()}
+              {/* {this.props.allProducts.map((product, ind)=>{    
+              return (<Row><Col sm = {12}><ProductCard  cproducts = {product} key={ind}/></Col></Row>)
+            })} */}
+            </Col>
+          </Row>
+          <Row>PageNation</Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+
+//fetching from store
+const mapStateToProps = (state) => {
+  return { allSellerProducts: state.allSellerProducts }
+}
+
+export default connect(mapStateToProps, { getALLProductsBySeller })(SellerHome);
+
