a838f15ae7c7992b1b83785513fc5901cf38606d
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-05-08 13:54:51 -0700

    modified order update api

--- No-Kafka-Backend/routers/write/seller/order/order.js
+++ No-Kafka-Backend/routers/write/seller/order/order.js
@@ -1,16 +1,19 @@
 const express=require('express');
 const router = express.Router();
 const Order = require('../../../../mongoModels/orders');
+const Sale = require('../../../../mongoModels/sales');
+const Purchase = require('../../../../mongoModels/customerPurchase');
 
 // Update order Status by Seller
 router.put('/:orderId', async (req, res) => {
     
-    const { orderStatus, orderUpdateItem, productId } = req.body; 
+    const { totalPrice, orderStatus, orderUpdateItem, productId } = req.body; 
     try {
         const order = await Order.findById({_id: req.params.orderId})
         if(!order) {
             return res.status(404).send('Order not found!');
         }
+        const {customerName, customerId} = order;
         order.products.map((product) => {
             if(product.productId === productId) {
                 if(orderStatus) {
@@ -19,6 +22,13 @@ router.put('/:orderId', async (req, res) => {
                 if(orderUpdateItem) {
                     const list = [orderUpdateItem, ...product.orderUpdates];
                     product.orderUpdates = list
+                }
+                if(totalPrice) {
+                    product.totalPrice = 0;
+                    const sale = new Sale({sellerName: product.sellerName, sales: -1*product.totalPrice})
+                    await sale.save();
+                    const purchase = new Purchase({customerId: customerId, customerName: customerName, purchase: -1*product.totalPrice});
+                    await purchase.save();
                 }
             }
         })
